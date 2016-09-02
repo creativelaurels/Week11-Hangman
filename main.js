f064f5b5993e642905fd3074b598de60fa4fef47
creativelaurels <lauren@creativelaurels.com>
2016-09-02 14:38:37 -0400

    Created letter function/constructor

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 // main.js
 // Contains the logic of the Hangman app. Running it in Terminal/Bash will start the game.
 // The app should end when a player guesses the correct word or runs out of guesses.
+
+// VARIABLES
+//Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 0;
